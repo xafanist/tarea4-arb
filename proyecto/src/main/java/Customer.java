9936fe16a19aa07c4fc882d6cf8c4d383a7a9d9b
xafan <toni.rb97@hotmail.com>
2022-03-18 13:20:08 +0100

    ## Refactorización 2

--- proyecto/src/main/java/Customer.java
+++ proyecto/src/main/java/Customer.java
@@ -22,22 +22,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) 
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            thisAmount = each.getCharge(thisAmount);
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -53,6 +38,7 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+
 
     public String getName() {
         return _name;
